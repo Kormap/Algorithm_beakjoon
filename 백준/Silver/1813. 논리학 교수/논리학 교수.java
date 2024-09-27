daba0de2e9fb53d402bd51b329a43f18e30d8631
Donghyeok Ko <101923048+Kormap@users.noreply.github.com>
2024-09-27 19:38:45 +0900

    Update 논리학 교수.java

--- 백준/Silver/1813. 논리학 교수/논리학 교수.java
+++ 백준/Silver/1813. 논리학 교수/논리학 교수.java
@@ -5,17 +5,21 @@ public class Main {
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(); //갯수
-        int counts[] = new int[51];
-        
-        int answer = 0;
 
+        // 0~50 까지 인덱스 생성
+        int counts[] = new int[51];
+
+        // counts 배열 자리에 카운트
         for(int i=0; i < N; i++){
             counts[sc.nextInt()]++;
         }
         
+        // 모순인 경우 -1
         int max = -1;
+
         for(int j=0; j < 51; j++){
-            if(counts[j] > max && counts[j] == j){
+            // 카운트가 잘됬는지 and 제일 큰 경우의 수인지 체크
+            if(counts[j] == j && counts[j] > max){
                 max = counts[j];
             }
         }
