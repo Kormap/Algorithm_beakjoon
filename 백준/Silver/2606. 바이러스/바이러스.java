feec12ea236fff2fb2addc47d72b0b81dada8b7c
Donghyeok Ko <101923048+Kormap@users.noreply.github.com>
2024-10-19 00:14:14 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 13412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,37 +1,40 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
-    static int N = sc.nextInt();   //컴퓨터의 개수
-    static int connect = sc.nextInt(); // 연결된 컴퓨터 쌍의 수
-    static int[][] linkComp = new int[N+1][N+1];
-    static boolean[] visited = new boolean[N+1];
+    static boolean[] visited;
+    static int n = 0;
+    static int[][] connectArr;
     static int answer = 0;
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
+        n = sc.nextInt();       //컴퓨터의 수
+        int connect = sc.nextInt(); //연결된 컴퓨터 쌍
 
-        for(int i=0; i<connect; i++) {
+        connectArr = new int[n + 1][n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 1; i <= connect; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            linkComp[x][y] = linkComp[y][x] = 1;
+
+            connectArr[x][y] = connectArr[y][x] = 1;
         }
 
-        //  1번 컴퓨터 부터 바이러스 검사
         bfs(1);
         System.out.println(answer);
     }
 
-    public static void bfs(int start){
-        visited[start] = true;
-
-        // 연결된 컴퓨터 개수만큼 탐색
-        for(int i=0; i<=N; i++){
-            if(linkComp[start][i] == 1 && !visited[i]){
+    public static void bfs(int x) {
+        visited[x] = true;
+        
+        for(int i=1; i<=n; i++){
+            if(connectArr[x][i] == 1 && !visited[i]){
+                visited[i] = true;
                 answer++;
                 bfs(i);
             }
         }
     }
+
 }
