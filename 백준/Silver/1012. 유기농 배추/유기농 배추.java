79f81b7c600cf638573fdcc5c15ee3afa2f19a26
Donghyeok Ko <101923048+Kormap@users.noreply.github.com>
2024-10-19 01:39:37 +0900

    [Silver II] Title: 유기농 배추, Time: 224 ms, Memory: 26964 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,73 +1,68 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
-    static int T = sc.nextInt();   //케이스
-    static int M;//가로
-    static int N;//세로
-    static int K;//배추 갯수
     static int map[][];
     static boolean visited[][];
-    static int count;
+    static int M;
+    static int N;
+    static int answer = 0;
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
+        int T = sc.nextInt();   //테스트케이스
 
-        for(int i = 0; i < T; i++) {
-            N = sc.nextInt();
-            M = sc.nextInt();
-            K = sc.nextInt();
-            map = new int[N][M];
-            visited = new boolean[N][M];
-            count = 0; //카운트 초기화
+        for (int i = 1; i <= T; i++) {
+            N = sc.nextInt();   //세로
+            M = sc.nextInt();   //가로
+            int K = sc.nextInt();   //배추가 심어져 있는 위치 개수
 
-            for(int j = 0; j < K; j++){
-                int x = sc.nextInt();
-                int y = sc.nextInt();
-                map[x][y] = 1;
+            map = new int[M][N];
+            visited = new boolean[M][N];
+
+            //정답 초기화
+            answer = 0;
+
+            for (int j = 1; j <= K; j++) {
+                int Y = sc.nextInt();   //Y좌표
+                int X = sc.nextInt();   //X좌표
+
+                map[X][Y] = 1;
             }
 
-            for(int j = 0; j < N; j++){
-                for(int k = 0; k < M; k++){
-                    //배추 존재유무, 체크 존재유무 확인 후 탐색
-                    if(map[j][k] == 1 && !visited[j][k]){
-                        bfs(j,k);
-                        count++;
+            for(int x = 0; x < M; x++) {
+                for(int y = 0; y < N; y++) {
+                    if (map[x][y] == 1 && !visited[x][y]) {
+                        bfs(x, y);
+                        answer++;
                     }
                 }
             }
-            System.out.println(count);
-        }
 
+            System.out.println(answer);
+        }
     }
 
-    public static void bfs(int x, int y){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x, y});   //x,y 좌표
+    public static void bfs(int x, int y) {
         visited[x][y] = true;
 
-        int[] dx = {0,0,-1,1};
-        int[] dy = {-1,1,0,0};
+        //상하좌우 탐색을 위한 x,y 좌표
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {1, -1, 0, 0};
 
-        while (!q.isEmpty()){
-            int[] cur = q.poll();
-
-            //상하좌우 4CASE
-            for(int i = 0; i < 4; i++){
-                int xPoint = cur[0];//x좌표
-                int yPoint = cur[1];//y좌표
-                xPoint += dx[i];
-                yPoint += dy[i];
-
-                //지도에 벗어나지 않은 경우만 체크
-                if(xPoint >= 0 && xPoint < N && yPoint >= 0 && yPoint < M){
-                    if(map[xPoint][yPoint] == 1 && !visited[xPoint][yPoint]){
-                        q.offer(new int[]{xPoint, yPoint});
-                        visited[xPoint][yPoint] = true;
-                    }
+        //상하좌우 네가지 케이스 탐색
+        for (int i = 0; i < 4; i++) {
+            int nowX = x + dx[i];
+            int nowY =  y + dy[i];
+            // 좌표 내부만 탐색
+            if (nowX >= 0 && nowX < M && nowY >= 0 && nowY < N) {
+                if (map[nowX][nowY] == 1 && !visited[nowX][nowY]) {
+                    visited[nowX][nowY] = true;
+                    bfs(nowX, nowY);
                 }
             }
         }
+
     }
+
+
 }
