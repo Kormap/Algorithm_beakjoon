15bd33495abbd135f2e96f632242fd0098c43ad1
Donghyeok Ko <101923048+Kormap@users.noreply.github.com>
2025-06-04 00:58:16 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 216 ms, Memory: 16764 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.regex.Pattern;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -9,41 +7,39 @@ public class Main {
 
         while (true) {
             String s = br.readLine();
-            if(".".equals(s)) break;
-            
+            if (".".equals(s))
+                break;
+
             Stack<Character> stack = new Stack<>();
             boolean flag = true;
 
             for (int i = 0; i < s.length(); i++) {
                 char c = s.charAt(i);
-
-                // 소괄호, 대괄호 시작일 경우
-                if(c == '(' || c == '[') {
+                if (c == '(' || c == '[') {
                     stack.push(c);
-                }
-                // 닫는 소괄호가 입력된 경우
-                else if (c == ')'){
-                    if(stack.isEmpty() || stack.peek() != '(') {
+                } else if (c == ')') {
+                    if (!stack.isEmpty() && stack.peek() == '(') {
+                        stack.pop();
+                    } else {
                         flag = false;
                         break;
-                    } else {
-                        stack.pop();
                     }
-                }
-                // 닫는 대괄호가 입력된 경우
-                else if (c == ']'){
-                    if(stack.isEmpty() || stack.peek() != '[') {
+                } else if (c == ']') {
+                    if (!stack.isEmpty() && stack.peek() == '[') {
+                        stack.pop();
+                    } else {
                         flag = false;
                         break;
-                    } else {
-                        stack.pop();
                     }
                 }
+
             }
 
-
-            if(flag && stack.isEmpty()) System.out.println("yes");
-            else System.out.println("no");
+            if (stack.isEmpty() && flag)
+                System.out.println("yes");
+            else
+                System.out.println("no");
         }
     }
+
 }
